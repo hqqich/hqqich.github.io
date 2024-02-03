88108669bf7fc9c83e0e3a3bb8ce8610094bebe8
hqqich <hqqich1314@outlook.com>
2024-02-03 20:01:47 +0800

    delete

--- blog/src/.vuepress/sidebar.ts
+++ blog/src/.vuepress/sidebar.ts
@@ -4,10 +4,10 @@ export default sidebar({
   "/": [
     "",
     {
-      text: "Lottery 项目开发日志",
+      text: "个人学习日志",
       icon: "laptop-code",
       prefix: "md/dev-log/",
-      children: ["day01.md","day02.md","day03.md","day04.md","day05.md","day06.md","day07~day08.md","day09~day10.md","day11.md","day12.md"],
+      children: ["day01.md"],
     },
   ],
 });
