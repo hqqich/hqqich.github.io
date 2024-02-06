8f580a762080690989abca1c6fd7c882bc04e550
hqqich <hqqich1314@outlook.com>
2024-02-06 15:53:08 +0800

    优化

--- blog/src/.vuepress/navbar.ts
+++ blog/src/.vuepress/navbar.ts
@@ -2,11 +2,16 @@
 import {navbar} from "vuepress-theme-hope";
 
 export default navbar([
-  "/",
-  "/md/resume",
-  {
-    text: "学习日志",
-    icon: "book",
-    link: "/md/dev-log/day01.md",
-  }
+    "/",
+    "/md/resume",
+    {
+        text: "学习日志",
+        icon: "book",
+        link: "/md/dev-log/day01.md",
+    },
+    {
+        text: "kotlin",
+        icon: "book",
+        link: "/md/kotlin/两万六千字带你 Kotlin 入门.md",
+    }
 ]);
